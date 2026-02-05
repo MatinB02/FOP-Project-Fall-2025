9bafa9bd9478e5099b24651837d7d1e0e38ae6ab
MatinB02 <matinbagheri402@gmail.com>
2026-02-05 19:01:27 +0330

    easiest FOP project of all time

---
+++ engine/core/vec2.h
@@ -0,0 +1,16 @@
+/**
+ * @file vec2.h
+ * @brief 2D Vector math for movement and positioning.
+ * * In this engine, every position and velocity is a Vec2. 
+ * Think of 'x' as horizontal and 'y' as vertical coordinates.
+ */
+
+#ifndef ENGINE_CORE_VEC2_H
+#define ENGINE_CORE_VEC2_H
+
+typedef struct Vec2 {
+    float x;
+    float y;
+} Vec2;
+
+#endif
